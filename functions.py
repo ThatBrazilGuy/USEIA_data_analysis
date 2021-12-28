7f958ead3d3aa26ea6870bc412628f9687ae858e
jczbrush <80019533+ThatBrazilGuy@users.noreply.github.com>
2021-12-28 08:37:15 -0800

    update

---
+++ functions.py
@@ -0,0 +1,21 @@
+    """This is a series of functions for organizing various data.
+    """
+
+from dotenv import load_dotenv
+import pandas as pd
+import json
+import numpy
+import seaborn 
+import matplotlib.pyplot as plt
+import requests
+import os
+
+def total_energy_production():
+    pass
+
+def category_ids():
+    pass 
+
+def state_series_ids(list):
+    pass 
+
